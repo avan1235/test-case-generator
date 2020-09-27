1c99dca7d909cbe5201468972cda0ad7bc434570
Maciej Procyk <macieekprocyk@gmail.com>
2020-09-27 22:26:13 +0200

    fixes linter

--- src/main/kotlin/com/procyk/maciej/tcgenerator/providers/jira/JiraConfiguration.kt
+++ src/main/kotlin/com/procyk/maciej/tcgenerator/providers/jira/JiraConfiguration.kt
@@ -1,8 +1,8 @@
 package com.procyk.maciej.tcgenerator.providers.jira
 
+import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.ServiceManager
-import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 
